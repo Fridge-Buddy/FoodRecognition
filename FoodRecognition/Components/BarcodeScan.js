025ca8e62112bfbffd1ad71cce22f6bae1891aa0
Andres Garcia <andresfg@bu.edu>
2023-12-09 02:04:56 -0500

    Barcode scanner working

---
+++ FoodRecognition/Components/BarcodeScan.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+
+export default function BarcodeScan() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    if (!scanned) {
+      console.log(`Bar code with type ${type} and data ${data} has been scanned!`);
+      setScanned(true);
+      setTimeout(() => setScanned(false), 3000); // 3 seconds delay
+    }
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <BarCodeScanner
+        onBarCodeScanned={handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+});
